aa47cb8a29a44b2c9b2137bf0090e15fb0527add
jduquem <alejandro.duque1104@gmail.com>
2024-04-18 20:26:09 -0500

    Model

--- Model.py
+++ Model.py
@@ -1,4 +1,5 @@
-class person(models.Model):
-    identification = models.ForeignKey(on_delete=models.CASCADE)
+class Person(models.Model):
+    identification = models.CharField(max_length=200, unique=True)
     name = models.CharField(max_length=200)
-    birthdate = models.DateTimeField(null=True)
+    birthdate = models.DateTimeField()
+    level = models.IntegerField()
