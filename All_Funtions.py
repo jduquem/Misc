13e8758b124f1d936ae34c1d7aba7c4a3c39ea06
alejo duque <alejandro.duque1104@gmail.com>
2024-04-19 11:17:23 -0500

    rename methods

--- All_Funtions.py
+++ All_Funtions.py
@@ -1,23 +1,25 @@
-# Numeros aleatoreos positivos del 1 al 20.000
 import random
 
-def NumberRandom(CantNumber):
+# Numeros aleatoreos positivos del 1 al 20.000
+def random_number(number_count):
     numeros_aleatorios = []
-    for _ in range(CantNumber):
+    for _ in range(number_count):
         numeros_aleatorios.append(random.randint(1, 20000))
     return numeros_aleatorios
 
-CantNumber = int(input('Ingrese la cantidad de numeros que desea ver: '))
-numeros_aleatorios = NumberRandom(CantNumber)
-print(numeros_aleatorios)
+
+def number_is_par(number):
+    if number %2 == 0:
+        result = True
+    elif number != 0:
+        result = False
+    return result
 
 
-def returBoolea(num):
-    if num %2 == 0:
-        Boovalue = True
-    elif num != 0:
-        Boovalue = False
-    return Boovalue
+def __init__():
+    number_count = int(input('Ingrese la cantidad de numeros que desea ver: '))
+    random_numbers = random_number(number_count)
+    print(random_numbers)
 
-num = int(input('Ingrese el nuemro: '))
-print(returBoolea(num))
+    num = int(input('Ingrese el numero: '))
+    print(number_is_par(num))
